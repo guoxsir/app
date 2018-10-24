3029f818ac39837fef84f817850f74c098a1bbf0
guox <492273770@qq.com>
2018-10-24 10:54:44 +0800

    Add files via upload

---
+++ static/js/jsConfig.js
@@ -0,0 +1,58 @@
+
+
+
+
+// 行方提供的数据信息
+
+export  default {
+
+  //本地后台接口域名
+
+  // 打包地址
+  'host_local_url': 'http://210.13.81.69:6702/jiaofei/',
+  // 山东分行
+  // 'host_local_url': 'https://cloud.bankofchina.com/sd/jiaofei/',
+  //  'host_local_url': 'http://localhost:8080/',
+  // 'host_local_url': 'http://192.168.43.37:8080/',
+  //
+
+  //行方提供clientid
+
+  //新意clientid
+  // 'clientid': 507,
+  //行方提供clientSecret
+  // 'clientSecret': '26ab01b20fdde286b0bde7c09ecbe36db0aea2a3689e9a06c7',
+
+  // // 陕西E校园clientid
+  // 'clientid': 428,
+  // 'clientSecret': '26ab01b20fdde286b0bde7c09ecbe36db0aea2a3689e9a06c7',
+
+
+  // 山东缴费clientid
+
+  'clientid' : 521,
+  'clientSecret' : '8d62a187cb2631ec5521781de882d20bed1e0a933ccc52528b',
+
+  //首页地址
+
+  //  'indexHref': 'http://192.168.43.37:8080/app/index',
+  //  'indexHref': 'http://localhost:8080/app/index',
+  // 山东分行
+  // 'indexHref': 'https://cloud.bankofchina.com/sd/jiaofei/app/index',
+  // 打包地址
+  'indexHref': 'http://210.13.81.69:6702/jiaofei/app/index',
+
+
+  // // 行方公共H5页面（网银和易商登录均有）
+  'bank_login': 'https://open.boc.cn/wap/login/dbLogin?',
+
+  // 行方公共H5页面（网银登录）
+  // 'bank_login': 'http://open.boc.cn/bocop/#/app/netLogin/single?',
+  //银行卡管理
+  'bank_card': 'https://open.boc.cn/wap/cardmange.php?&',
+  //修改登录密码
+  'change_login_pass': 'https://open.boc.cn/wap/pwdedit.php?',
+  //修改支付密码
+  'get_pay_pwd': 'https://open.boc.cn/wap/payPwd/getPayPwd?'
+}
+
