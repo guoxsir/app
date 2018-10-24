46a795b93054827a23a1a1dd91499c5bd867a377
惮晰 <492273770@qq.com>
2018-10-24 11:12:14 +0800

    xxxxx

--- static/js/iscroll.js
+++ static/js/iscroll.js
@@ -6,7 +6,6 @@ var rAF = window.requestAnimationFrame	||
 	window.oRequestAnimationFrame		||
 	window.msRequestAnimationFrame		||
 	function (callback) { window.setTimeout(callback, 1000 / 60); };
-
 var utils = (function () {
 	var me = {};
 
